81c64fcec8e669092f6098d212939b147992716b
alberth <maikeru.bash@gmail.com>
2018-05-02 18:24:06 -0400

    codigos

---
+++ scripts_mysql/enlace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enlace : MonoBehaviour {
+
+	public string web;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void ir_a_web(){
+		Application.OpenURL (web);
+	}
+}
